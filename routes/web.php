44be9ac729ab5457273669290a31d79c6f6a97a8
fpizelli <168946758+fpizelli@users.noreply.github.com>
2024-11-14 01:38:04 -0300

    Finish day 13

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,17 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Models\Job;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 });
 
 Route::get('/jobs', function () {
+    $jobs = Job::with('employer')->get();
+
     return view('jobs', [
-        'jobs' => Job::all()
+        'jobs' => $jobs
     ]);
 });
 
